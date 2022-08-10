df55b3dddcb863e0a9ddb8b34fc8e64872d1701a
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2022-08-10 17:56:09 -0600

    Refactored components, needs styled components installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { getRestaurants } from './utils/serverCalls';
+import  { Home, Results }  from './pages'
 
-function App() {
+const App = () => {
 
   async function test() {
     const payload = {
@@ -20,23 +20,11 @@ function App() {
   
   // console.log(test())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home/>
+      <Results/>
     </div>
-  );
+  )
 }
 
 export default App;

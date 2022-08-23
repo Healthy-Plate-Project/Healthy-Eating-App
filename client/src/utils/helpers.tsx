bd3c1feabdcad82f9233dd2e3e4c2cba1f6797fb
John W. Davis <johndavis92790@gmail.com>
2022-08-22 21:23:04 -0600

    Create convertMetersToMiles() & convertMilesToMeters() helper functions (#65)

--- client/src/utils/helpers.tsx
+++ client/src/utils/helpers.tsx
@@ -1,4 +1,4 @@
-export function apiServer() {
+function apiServer() {
   const url = window.location.href;
   const slice = url.slice(0, 21);
   if (slice === 'http://localhost:3000') {
@@ -6,4 +6,18 @@ export function apiServer() {
   } else {
     return 'https://healthy-eating-project-359101.uc.r.appspot.com';
   }
+}
+
+function convertMetersToMiles(meters: number) {
+  return meters * 0.000621371192;
+}
+
+function convertMilesToMeters(miles: number) {
+  return miles * 1609.344;
+}
+
+export {
+  apiServer,
+  convertMetersToMiles,
+  convertMilesToMeters
 }

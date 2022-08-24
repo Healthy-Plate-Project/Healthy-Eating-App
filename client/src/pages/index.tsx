d4cccc4934c0a58734f7dc89b4293a3daf499eaf
Darolo13 <davidromero1244@gmail.com>
2022-08-23 21:27:49 -0700

    :truck: use index to avoid redundant imports

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Results from "./Results/Results";
-import Home from "./Home/Home";
+import Results from "./Results";
+import Home from "./Home";
 import NotFound from "./NotFound";
 
 export { Results, Home, NotFound };

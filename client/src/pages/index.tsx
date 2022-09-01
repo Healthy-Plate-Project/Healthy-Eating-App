196d37bedb5c2b1d739df71aa817b308ca01beef
robyng <robyngraham01@gmail.com>
2022-09-01 13:49:09 -0700

    change file name of /Review/index.tsx to /Review/Review.tsx and same for NotFound. Update /Pages/index.tsx file paths

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Results from "./Results/Results";
 import Home from "./Home/Home";
-import NotFound from "./NotFound";
-import Review from "./Review";
+import NotFound from "./NotFound/NotFound";
+import Review from "./Review/Review";
 
 export { Results, Home, NotFound, Review };

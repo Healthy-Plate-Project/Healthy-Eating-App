d7a4940fddb498ab1a2ed7d2a0272f4b00f6cccd
John Davis <johndavis92790@gmail.com>
2022-09-07 18:37:49 -0600

    test

---
+++ client/src/pages/Review/Review.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import VeganStarRating from "./VeganStarRating";
+
+const Review = () => {
+
+  const [selectedVeganStarRating, setSelectedVeganStarRating] = useState<number>();
+  const [selectedVegetarianStarRating, setSelectedVegetarianStarRating] = useState<number>();
+  const [selectedPescatarianStarRating, setSelectedPescatarianStarRating] = useState<number>();
+  const [selectedGlutenFreeStarRating, setSelectedGlutenFreeStarRating] = useState<number>();
+  const [selectedDairyFreeStarRating, setSelectedDairyFreeStarRating] = useState<number>();
+  const [selectedNutFreeStarRating, setSelectedNutFreeStarRating] = useState<number>();
+  
+
+  
+
+  return (
+    <div>
+      <h1>Review for Business Name Placeholder</h1>
+      <form className="stars">
+        <h3>Rate this restaurant based on the special diet:</h3>
+        <VeganStarRating
+          selectedVeganStarRating={selectedVeganStarRating}
+          setSelectedVeganStarRating={setSelectedVeganStarRating}
+        >
+        </VeganStarRating>
+
+        <h3>How was your experience?</h3>
+        <textarea className="paragraph" name="review" aria-label="review" id="review" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default Review;

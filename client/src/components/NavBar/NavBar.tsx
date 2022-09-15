6e61673a9379c31e67e8d10321af24217c241b7e
John Davis <johndavis92790@gmail.com>
2022-09-14 19:19:49 -0600

    temp fix

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { convertMilesToMeters } from "../../utils/helpers";
-import { getRestaurant, getRestaurants, saveRestaurant } from "../../utils/serverCalls";
+// import { getRestaurant, getRestaurants, saveRestaurant } from "../../utils/serverCalls";
 import NavbarStyled from "./NavbarStyles";
 
 export function Navbar(props: any) {
@@ -15,38 +15,38 @@ export function Navbar(props: any) {
     props.setCurrentUserEmail('');
   }
 
-  async function testAPICalls() {
-    const getRestaurantsResults = await getRestaurants({
-      latitude: '40.4865496',
-      longitude: '-111.9164662',
-      keyword: 'thai',
-      maxPrice: 3,
-      minPrice: 2,
-      openNow: true,
-      radius: convertMilesToMeters(10),
-      // TODO: use this to setup pagination
-      // pageToken: req.pageToken,
-    })
-    const getRestaurantResults = await getRestaurant(
-      'ChIJn58N1B9gUocRpAXOXPbFcOo'
-    )
-    const saveRestaurantResults = await saveRestaurant({
-      place_id: 'testID1',
-      restaurantName: 'testName',
-      longitude: 'testLong',
-      latitude: 'testLat',
-      photoReference: 'testPhoto',
-    })
-    console.log(getRestaurantsResults.results)
-    console.log(getRestaurantResults.result)
-    console.log(saveRestaurantResults)
-  }
+  // async function testAPICalls() {
+  //   const getRestaurantsResults = await getRestaurants({
+  //     latitude: '40.4865496',
+  //     longitude: '-111.9164662',
+  //     keyword: 'thai',
+  //     maxPrice: 3,
+  //     minPrice: 2,
+  //     openNow: true,
+  //     radius: convertMilesToMeters(10),
+  //     // TODO: use this to setup pagination
+  //     // pageToken: req.pageToken,
+  //   })
+  //   const getRestaurantResults = await getRestaurant(
+  //     'ChIJn58N1B9gUocRpAXOXPbFcOo'
+  //   )
+  //   const saveRestaurantResults = await saveRestaurant({
+  //     place_id: 'testID1',
+  //     restaurantName: 'testName',
+  //     longitude: 'testLong',
+  //     latitude: 'testLat',
+  //     photoReference: 'testPhoto',
+  //   })
+  //   console.log(getRestaurantsResults.results)
+  //   console.log(getRestaurantResults.result)
+  //   console.log(saveRestaurantResults)
+  // }
 
   return (
     <NavbarStyled>
       <Link to="/">Home</Link>
       <Link to="results">Results</Link>
-      <button onClick={testAPICalls}>Test API Calls</button>
+      {/* <button onClick={testAPICalls}>Test API Calls</button> */}
     </NavbarStyled>
   )
 }

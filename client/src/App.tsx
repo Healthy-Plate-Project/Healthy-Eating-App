5cd6991eb09b099be200e036cb3b04f9fc27703b
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2022-09-14 22:00:40 -0600

    Change file names for results. Style navbar.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom'
-import {
-  Routes,
-  Route,
-} from "react-router";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import {
   Home,
-  Results,
+  Result,
   NotFound,
   Login,
   Review,
@@ -17,19 +14,16 @@ import GlobalStyle from "./theme/globalStyle";
 import { Navbar } from "./components";
 
 const App = () => {
-
-  const [currentUserEmail, setCurrentUserEmail] = useState('');
+  const [currentUserEmail, setCurrentUserEmail] = useState("");
   useEffect(() => {
-    (
-      async () => {
-        const response = await fetch("/api/user/user", {
-          headers: { "Content-Type": "application/json" },
-          credentials: 'include'
-        });
-        const content = await response.json();
-        setCurrentUserEmail(content.username);
-      }
-    )();
+    (async () => {
+      const response = await fetch("/api/user/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const content = await response.json();
+      setCurrentUserEmail(content.username);
+    })();
   });
 
   return (
@@ -39,13 +33,24 @@ const App = () => {
         <Routes>
           {/* <Route exact path="/" element={<App />} /> */}
           <Route path="/" element={<Home />} />
-          <Route path="results" element={<Results />} />
+          <Route path="results" element={<Result />} />
           <Route path="review" element={<Review />} />
           <Route path="reviews" element={<ReviewsListParent />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="login" element={<Login currentUserEmail={currentUserEmail} setCurrentUserEmail={setCurrentUserEmail} />} />
+          <Route
+            path="login"
+            element={
+              <Login
+                currentUserEmail={currentUserEmail}
+                setCurrentUserEmail={setCurrentUserEmail}
+              />
+            }
+          />
         </Routes>
-        <Navbar currentUserEmail={currentUserEmail} setCurrentUserEmail={setCurrentUserEmail} />
+        <Navbar
+          currentUserEmail={currentUserEmail}
+          setCurrentUserEmail={setCurrentUserEmail}
+        />
       </BrowserRouter>
     </div>
   );

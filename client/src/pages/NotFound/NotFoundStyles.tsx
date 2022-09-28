4ce04a3368baa03d045495d560b09f588d790dee
robyng <robyngraham01@gmail.com>
2022-09-28 16:00:21 -0700

    add wrapper styles to NotFound

---
+++ client/src/pages/NotFound/NotFoundStyles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  h1 {
+    margin: 20px 0;
+  }
+  h2 {
+    margin: 18px 0;
+  }
+  h3 {
+    margin: 15px 0;
+  }
+`;

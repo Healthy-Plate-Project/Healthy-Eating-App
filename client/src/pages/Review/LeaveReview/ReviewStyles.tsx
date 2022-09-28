31f80295907d8925b375fd4397239b1d727b7e7e
robyng <robyngraham01@gmail.com>
2022-09-28 15:41:53 -0700

    Wrapper styled component

--- client/src/pages/Review/LeaveReview/ReviewStyles.tsx
+++ client/src/pages/Review/LeaveReview/ReviewStyles.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 /*paragraph input on forms*/
-const Textarea = styled.textarea`
+export const Textarea = styled.textarea`
   width: 100%;
-  height: 150px;
-`; 
+  height: 50px;
+`;
 
-export default Textarea;
+export const Wrapper = styled.div`
+  h1 {
+    margin: 20px 0;
+  }
+  h2 {
+    margin: 18px 0;
+  }
+  h3 {
+    margin: 15px 0;
+  }
+`;

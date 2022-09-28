5643069c9a9368472fb2d088974a02f468e1704c
John Davis <johndavis92790@gmail.com>
2022-09-27 22:08:54 -0600

    Wire up search result and single result page to api.

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -94,7 +94,6 @@ export function Navbar(props: any) {
       <Link to="review">Review</Link>
       <Link to="reviews">Reviews</Link>
       <StyledButton onClick={() => testAPICalls()}>Test API</StyledButton>
-      <Link to={"/single-result/ChIJn58N1B9gUocRpAXOXPbFcOo"}>Single Result</Link>
     </NavbarStyled>
   );
 }

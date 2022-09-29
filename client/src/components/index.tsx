2db018d050eabd864bdba8bfd717ebfe2c0460fd
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2022-09-28 19:38:38 -0600

    fixed capitalization issues on my end

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,6 @@
-import Search from "./search/Search";
+import Search from "./Search/Search";
 import Button from "./Button/Button";
-import Input from "./input/Input";
+import Input from "./Input/Input";
 import { Navbar } from "./NavBar/NavBar";
 
 export { Navbar, Search, Button, Input };

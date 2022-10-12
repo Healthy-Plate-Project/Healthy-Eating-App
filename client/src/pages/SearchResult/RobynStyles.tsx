8d4ed893162dd99684821457915bbd1fb7391dcf
robyng <robyngraham01@gmail.com>
2022-10-12 16:57:54 -0700

    add dollar sign symbol and styling

--- client/src/pages/SearchResult/RobynStyles.tsx
+++ client/src/pages/SearchResult/RobynStyles.tsx
@@ -4,7 +4,20 @@ export const H1 = styled.h1`
   margin: 20px 0;
 `;
 
+export const H3 = styled.h3`
+  margin: 15px 0;
+`;
+
 export const HeartEmpty = styled.img`
   margin: 0 10px;
   height: 20px;
+`;
+
+export const Dollar = styled.img`
+  margin: 0 1px;
+  height: 15px;
+`;
+
+export const Wrapper = styled.div`
+  text-align: left;
 `;
--- client/src/pages/SearchResult/SingleSearchResult.tsx
+++ client/src/pages/SearchResult/SingleSearchResult.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRestaurant } from "../../utils/serverCalls";
-import { H1, HeartEmpty } from "./RobynStyles";
+import { H1, H3, HeartEmpty, Wrapper, Dollar } from "./RobynStyles";
 import heartEmpty from "../../assets/images/heart-empty.png";
+import dollarEmpty from "../../assets/images/dollar-empty.png";
+import dollarFilled from "../../assets/images/dollar-filled.png";
 import HeartFavorite from "./HeartFavorite";
+import { Button } from "../../components";
 
 export interface SingleRestaurantData {
   name: string;
@@ -60,21 +63,48 @@ export function SingleSearchResultPage() {
 
   return (
     <>
-      <H1>
-        {restaurantData.name}
-        <HeartFavorite
-          selectedHeart={selectedHeart}
-          setSelectedHeart={setSelectedHeart}
-        ></HeartFavorite>
-      </H1>
-      <p>Restaurant Place_id: {restaurantData.place_id}</p>
-      {/* add $$$$ symbol */}
-      <p>Restaurant Price Level: {restaurantData.price_level}</p>
-      <p>Restaurant Address: {restaurantData.vicinity}</p>
-      <p>Restaurant Phone: {restaurantData.formatted_phone_number}</p>
-      <p>Restaurant Average Rating: {restaurantData.rating}</p>
-      <p>Restaurant Maps URL: {restaurantData.url}</p>
-      <p>Restaurant Website URL: {restaurantData.website}</p>
+      <Wrapper>
+        <H1>
+          {restaurantData.name}
+          <HeartFavorite
+            selectedHeart={selectedHeart}
+            setSelectedHeart={setSelectedHeart}
+          ></HeartFavorite>
+        </H1>
+        {/* <p>Restaurant Place_id: {restaurantData.place_id}</p> */}
+        {/* add $$$$ symbol */}
+
+        <p>
+          {restaurantData.price_level}
+          <Dollar src={dollarFilled} />
+          <Dollar src={dollarEmpty} />
+          <Dollar src={dollarEmpty} />
+          <Dollar src={dollarEmpty} />
+        </p>
+        <p>
+          <a href={restaurantData.url} rel="noreferrer" target="_blank">
+            {restaurantData.vicinity}
+          </a>
+        </p>
+        <p>{restaurantData.formatted_phone_number}</p>
+        <p>Google Rating: {restaurantData.rating}</p>
+        <p>Healthy App Rating:</p>
+        <p>
+          Restaurant Website URL:{" "}
+          <a href={restaurantData.website} rel="noreferrer" target="_blank">
+            {restaurantData.website}
+          </a>
+        </p>
+        <div>
+          <H3>Photos</H3>
+          <hr />
+        </div>
+        <div>
+          <H3>User Reviews</H3>
+          <hr />
+          <button>Write Review</button>
+        </div>
+      </Wrapper>
     </>
   );
 }

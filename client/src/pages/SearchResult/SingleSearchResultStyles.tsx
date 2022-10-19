8a313ff264170ac9f570f9e7881fa29de5d7a2e2
robyng <robyngraham01@gmail.com>
2022-10-18 21:46:03 -0700

    remove old styles for heart empty

--- client/src/pages/SearchResult/SingleSearchResultStyles.tsx
+++ client/src/pages/SearchResult/SingleSearchResultStyles.tsx
@@ -8,11 +8,6 @@ export const H3 = styled.h3`
   margin: 15px 0;
 `;
 
-export const HeartEmpty = styled.img`
-  margin: 0 10px;
-  height: 20px;
-`;
-
 export const Dollar = styled.img`
   margin: 0 1px;
   height: 15px;

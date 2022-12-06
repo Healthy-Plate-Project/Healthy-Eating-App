7bb4b9201b359549e736fd30ccc93d80ec8ff016
John Davis <johndavis92790@gmail.com>
2022-12-05 19:23:49 -0700

    Fix restaurant photos array and rendering.

--- client/src/pages/SearchResult/SingleSearchResult.tsx
+++ client/src/pages/SearchResult/SingleSearchResult.tsx
@@ -13,9 +13,11 @@ import heartEmpty from "../../assets/images/heart-empty.svg";
 import heartFilled from "../../assets/images/heart-filled.svg";
 import dollarFilled from "../../assets/images/green-dollar.svg";
 import GooglePhoto from "../../components/Photo/Photo";
+
 interface RestaurantPhoto {
   photo_reference: string;
 }
+
 export interface SingleRestaurantData {
   name: string;
   photo_reference: string;
@@ -53,18 +55,11 @@ export function SingleSearchResultPage() {
     {} as SingleRestaurantData
   );
 
-  const [restaurantPhotos, setRestaurantPhotos] = useState(
-    [] as RestaurantPhoto[]
-  );
-
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await getRestaurant(place_id);
         setRestaurantData(data.result);
-
-        setRestaurantPhotos(data.result.photos);
-
         // TODO: add heart api later
       } catch (err) {
         console.log(err);
@@ -72,81 +67,79 @@ export function SingleSearchResultPage() {
     }
     fetchData();
   }, [place_id]);
-  console.log(restaurantPhotos);
+
   // look at this object in the console to see what data is available to use
   // console.log(restaurantData);
 
   // default should be no, not favorited. use boolean type. True is selected.
   const [heart, setHeart] = useState<boolean>(false);
 
-  // dollar
-  let dollarAPI = restaurantData.price_level;
-
-  function priceLevel() {
+  function renderPriceLevel() {
     const array = [];
+    const dollarAPI = restaurantData.price_level;
     for (let i = 1; i <= dollarAPI; i++) {
       array.push(<PriceIconStyled src={dollarFilled} />);
     }
     return array;
   }
 
-  function getRestaurantPhotos() {}
+  function renderRestaurantPhotos(): any {
+    return Array.isArray(restaurantData.photos)
+      ? restaurantData.photos.map((photo, i) => (
+          <GooglePhoto
+            photo_reference={photo.photo_reference}
+            max_height="200"
+            max_width="200"
+            alt={`${restaurantData.name} - Photo# ${i + 1}`}
+            key={`${restaurantData.name} - ${i}`}
+          ></GooglePhoto>
+        ))
+      : null;
+  }
 
   return (
-    <>
+    <Wrapper>
       <GooglePhoto
         photo_reference={restaurantData.photo_reference}
         max_height="500"
         max_width="500"
-        alt="Test Photo"
+        alt={restaurantData.name}
       ></GooglePhoto>
-      <Wrapper>
-        <H1>
-          {restaurantData.name}
-          <span onClick={() => setHeart((prevState) => !prevState)}>
-            {heart ? (
-              <HeartIcon src={heartFilled} />
-            ) : (
-              <HeartIcon src={heartEmpty} />
-            )}
-          </span>
-        </H1>
-        <PriceContainer>{priceLevel()}</PriceContainer>
-        <p>
-          <a href={restaurantData.url} rel="noreferrer" target="_blank">
-            {restaurantData.vicinity}
-          </a>
-        </p>
-        <p>{restaurantData.formatted_phone_number}</p>
-        <p>Google Rating: {restaurantData.rating}</p>
-        <p>Healthy App Rating:</p>
-        <p>
-          Restaurant Website URL:{" "}
-          <a href={restaurantData.website} rel="noreferrer" target="_blank">
-            {restaurantData.website}
-          </a>
-        </p>
-        <div>
-          <H3>Photos</H3>
-          <hr />
-          <>
-            {restaurantPhotos.map((photo, i) => {
-              <GooglePhoto
-                photo_reference={photo.photo_reference}
-                max_height="200"
-                max_width="200"
-                alt="test"
-                key={`${restaurantData.name} - ${i}`}
-              ></GooglePhoto>;
-            })}
-          </>
-        </div>
-        <div>
-          <H3>User Reviews</H3>
-          <hr />
-          <button>Write Review</button>
-        </div>
-      </Wrapper>
-    </>
+      <H1>
+        {restaurantData.name}
+        <span onClick={() => setHeart((prevState) => !prevState)}>
+          {heart ? (
+            <HeartIcon src={heartFilled} />
+          ) : (
+            <HeartIcon src={heartEmpty} />
+          )}
+        </span>
+      </H1>
+      <PriceContainer>{renderPriceLevel()}</PriceContainer>
+      <p>
+        <a href={restaurantData.url} rel="noreferrer" target="_blank">
+          {restaurantData.vicinity}
+        </a>
+      </p>
+      <p>{restaurantData.formatted_phone_number}</p>
+      <p>Google Rating: {restaurantData.rating}</p>
+      <p>Healthy App Rating:</p>
+      <p>
+        Restaurant Website URL:{" "}
+        <a href={restaurantData.website} rel="noreferrer" target="_blank">
+          {restaurantData.website}
+        </a>
+      </p>
+      <div>
+        <H3>Photos</H3>
+        <hr />
+        {renderRestaurantPhotos()}
+      </div>
+      <div>
+        <H3>User Reviews</H3>
+        <hr />
+        <button>Write Review</button>
+      </div>
+    </Wrapper>
   );
 }

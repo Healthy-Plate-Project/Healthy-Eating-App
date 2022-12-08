08135abe8e7c88ee828a3f9da42dc7824db52140
John Davis <johndavis92790@gmail.com>
2022-12-07 20:30:15 -0700

    Change api data to test data

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const db = require("./config/connection");
 const app = express();
 
-exports.testAPI = false;
+exports.testAPI = true;
 
 // app.use(cookieParser());
 

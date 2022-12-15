0707b076b1e79031700a61bd3ab0511c2d6a1fb9
John Davis <johndavis92790@gmail.com>
2022-12-14 22:24:03 -0700

    Add missing brackets to GooglePhoto in RestaurantResults.tsx

--- client/src/components/RestaurantsResults/RestaurantsResults.tsx
+++ client/src/components/RestaurantsResults/RestaurantsResults.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { getRestaurants } from "../../utils/serverCalls";
-import GooglePhoto from "../Photo/Photo";
+import { GooglePhoto } from "../Photo/Photo";
 
 import {
   CardStyled,

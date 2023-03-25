c3283cdc5fd72b589567019e410c0abd471751c4
John W. Davis <jwdavis927@gmail.com>
2023-03-24 22:21:53 -0600

    Heroku setup

--- server/index.js
+++ server/index.js
@@ -33,7 +33,6 @@ if (process.env.NODE_ENV === "production") {
   router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
-  app.use(routes);
 }
 
 db.once("open", () => {
